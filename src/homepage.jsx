079a75f0fc0dec67bc2db367d50cf25043581d85
Ronnie <ronnielangat2020@gmail.com>
2024-05-24 09:52:26 +0300

    added the font family

---
+++ src/homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-900 font-darker-grotesque">Hello, World!</h1>
+    </div>
+  );
+}
+
+export default Homepage;
