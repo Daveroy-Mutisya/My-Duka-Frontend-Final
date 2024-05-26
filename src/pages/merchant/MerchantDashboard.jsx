ecd63ddf79956b9060910afdd2443fe92b34eddb
Ronnie <ronnielangat2020@gmail.com>
2024-05-26 10:51:04 +0300

    finished the dashboard

--- src/pages/merchant/MerchantDashboard.jsx
+++ src/pages/merchant/MerchantDashboard.jsx
@@ -9,7 +9,7 @@ export default function MerchantDashboard() {
 
     useEffect(() => {
         const token = localStorage.getItem('token');
-        fetch(`http://127.0.0.1:5000//stores`, {
+        fetch(`http://127.0.0.1:5000/stores`, {
             headers: {
                 'Authorization': `Bearer ${token}`,
             }
@@ -86,16 +86,23 @@ export default function MerchantDashboard() {
     };
 
     return (
-        <div className="container mx-auto p-4">
-            <h2 className="text-2xl font-bold mb-4">Merchant Dashboard</h2>
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="container-2xl bg-white p-6 rounded-lg shadow-md mx-4 sm:mx-auto w-full sm:w-3/4 lg:w-1/2 xl:w-1/3 min-h-[500px]">
+            <div className="flex items-center justify-center mb-4">
+                <h1 className="text-black text-3xl font-light font-darker-grotesque">MyDuk</h1>
+                <span className="text-red-600 text-3xl font-light font-darker-grotesque ml">A</span>
+            </div>
+            <div className='flex items-center justify-center mb-4'>
+                <h2 className="text-xl font-Normal mb-4">Merchant Dashboard</h2>
+            </div>
             <div className="mb-4">
-                <label htmlFor="store-select" className="block text-sm font-medium text-gray-700">Select Store</label>
+                <label htmlFor="store-select" className="block text-sm font-medium text-gray-700 font-darker-grotesque">Select Store</label>
                 <select
                     id="store-select"
                     onChange={(e) => handleStoreSelect(e.target.value)}
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    className="mt-1 font-medium block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm hover:outline-red focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-red-600 text-medium hover:bg-red-700 hover:text-red hover:bg-opacity-20"
                 >
-                    <option value="">Select a store</option>
+                    <option value="">.....</option>
                     {stores.map((store) => (
                         <option key={store.id} value={store.id}>
                             {store.name}
@@ -128,5 +135,9 @@ export default function MerchantDashboard() {
                 </div>
             )}
         </div>
+    </div>
+    
     );
 }
+
+ 
